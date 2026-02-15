981f062cd31261740ab6e783477f90d7bbdb5903
MRVEERAA <vasanthakumarssp@gmail.com>
2026-02-16 01:54:12 +0530

    Production Ready

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,33 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  // Cloud database (Render / Supabase)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    logging: false,
-  },
-);
+    logging: false, //  Hi sir If you Want See The Sequalize Log Change to True
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  //  Local database
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false, //  Hi sir If you Want See The Sequalize Log Change to True
+    },
+  );
+}
 
 module.exports = sequelize;
